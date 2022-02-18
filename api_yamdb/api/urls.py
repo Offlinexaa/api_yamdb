d26a5a802ad9d5a7d2acb640f8d9f4875ec9c24d
Offlinexaa <Offlinexaa@gmail.com>
2022-02-18 22:29:32 +0400

    Добавлена простая реализация JWT

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,11 @@
 """Модуль управления путями для api."""
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 from .views import UserViewSet
 
@@ -11,5 +16,8 @@ v1_router = SimpleRouter()
 v1_router.register('users', UserViewSet)
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('', include(v1_router.urls))
 ]
